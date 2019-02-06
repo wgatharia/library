d57da9dcdab7a4e3be0018497159932551a38d9a
wgatharia <wgatharia@yahoo.com>
2019-02-06 11:18:48 -0800

    added ejs as view engine

--- app.js
+++ app.js
@@ -13,10 +13,10 @@ app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dis
 app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
 app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist/js')));
 app.set('views', './src/views');
-app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  res.render('index', { list: ['a', 'b'] });
+  res.render('index', { list: ['a', 'b'], title: 'Library' });
 });
 
 app.listen(port, () => {
