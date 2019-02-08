585420f838a1915ea98a1ab5d04cc14d8d234531
wgatharia <wgatharia@yahoo.com>
2019-02-08 07:43:24 -0800

    fixed typos.

--- src/routes/booksRouter.js
+++ src/routes/booksRouter.js
@@ -1,31 +1,31 @@
 const express = require('express');
+
 const bookRouter = express.Router();
 
 const books = [{
-    title: 'Testing Python',
-    author: 'William Gatharia'
+  title: 'Testing Python',
+  author: 'William Gatharia'
 }];
 bookRouter.route('/')
-    .get((req, res) => {
-        res.render('bookListView', {
-            nav: [{ link: '/books', title: 'Books' },
-            { link: '/authors', title: 'Authors' }],
-            title: 'Library',
-            books
-        });
+  .get((req, res) => {
+    res.render('bookListView', {
+      nav: [{ link: '/books', title: 'Books' },
+        { link: '/authors', title: 'Authors' }],
+      title: 'Library',
+      books
     });
+  });
 
 bookRouter.route('/:id')
-    .get((req, res) => { 
-        const id  = req.param.id;
-        console.log(id);
-        res.render('bookView', {
-            nav: [{ link: '/books', title: 'Books' },
-            { link: '/authors', title: 'Authors' }],
-            title: 'Library',
-            book: id
-        });
+  .get((req, res) => {
+    const { id } = req.params;
+    res.render('bookView', {
+      nav: [{ link: '/books', title: 'Books' },
+        { link: '/authors', title: 'Authors' }],
+      title: 'Library',
+      book: books[id]
     });
+  });
 
 
 module.exports = bookRouter;
