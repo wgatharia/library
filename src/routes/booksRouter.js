00659fa3fce3a4ebf86afd9ae82335df4f1b8011
wgatharia <wgatharia@yahoo.com>
2019-02-10 18:20:28 -0800

    connected books to sql

--- src/routes/booksRouter.js
+++ src/routes/booksRouter.js
@@ -1,24 +1,34 @@
 const express = require('express');
-
+const sql = require('mssql');
+const debug = require('debug')('app:bookRouter');
 const bookRouter = express.Router();
 
 function router(nav) {
-  const books = [{
-    title: 'Testing Python',
-    author: 'William Gatharia'
-  }];
+  const books = [];
+
   bookRouter.route('/')
     .get((req, res) => {
-      res.render('bookListView', {
-        nav,
-        title: 'Library',
-        books
+      const request = new sql.Request();
+      request.query('select * from books').then(result => {
+        result.recordset.forEach((r) =>{
+          books.push({
+            title: r.title,
+            id: r.id,
+            author: r.author
+          })
+        });
+        
+        res.render('bookListView', {
+          nav,
+          title: 'Library',
+          books: result.recordset
+        });
       });
     });
 
   bookRouter.route('/:id')
     .get((req, res) => {
-      const { id } = req.params;
+      const id = parseInt(req.params.id) - 1;
       res.render('bookView', {
         nav,
         title: 'Library',
